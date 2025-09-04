346706bdc4ba60373cc3b5226c5e791348f6cb55
ddyural <84669233+ddyural@users.noreply.github.com>
2025-09-04 10:43:32 +0300

    EnterConfig
    We can enter the configuration on screen2

--- drafts/test2.py
+++ drafts/test2.py
@@ -4,11 +4,14 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-#from kivy.properties import ListProperty
+
 from kivy.uix.popup import Popup
 import webbrowser
 from kivy.core.window import Window
 from kivy.uix.widget import Widget as Spacer
+
+from kivy.properties import StringProperty
+#from kivy.properties import ListProperty
 
 
 class MainScreen(Screen):
@@ -57,6 +60,7 @@ class MainScreen(Screen):
     def go_to_screen2(self, instance):
         #self.data = self.config
         self.manager.get_screen("screen2").config = list(self.config)
+        self.manager.get_screen("screen2").lbl3.text = f"Текущая конфигурация: {self.config}"
         self.manager.current = 'screen2'
 
 
@@ -64,7 +68,6 @@ class Screen2(Screen):
     def __init__(self, **kwargs):
         super(Screen2, self).__init__(**kwargs)
         self.config = []
-
 
         self.rule = []
 
@@ -77,18 +80,18 @@ class Screen2(Screen):
         lbl = Label(text="Ввод правила по одному символу", font_size='30sp', color=[0, 1, 0, 1])
         lbl2 = Label(text="текущий формат: δ(q, a) = (b, D, p)\nдопустимые значения: δ(q¹, 0/1) = (0/1, R/L, q¹/q⁰)",
                      font_size='30sp', color=[0, 1, 0, 1])
-        lbl3 = Label(text=f"Текущая конфигурация: {self.config}", font_size='30sp', color=[0, 1, 0, 1])
+        self.lbl3 = Label(text=f"Текущая конфигурация: {self.config}", font_size='30sp', color=[0, 1, 0, 1])
         self.lbl4 = Label(text=f"Текущее правило: {self.rule}", font_size='30sp', color=[0, 1, 0, 1])
         self.lbl5 = Label(text="введите, где стоит xxx: δ(q1, xxx) = (*, *, *): ", font_size='30sp', color=[0, 1, 0, 1])
 
         btn1 = Button(text="Ввод", font_size='20sp', background_color=[0, 1, 0, 1], height=100)
-        btn1.bind(on_press=self.inputtext)
+        btn1.bind(on_press=self.enter_rule())
 
-        btn2 = Button(text="Конец ввода", font_size='20sp', background_color=[0, 1, 0, 1], height=100)
+        btn2 = Button(text="Применить правило", font_size='20sp', background_color=[0, 1, 0, 1], height=100)
 
         layout.add_widget(lbl)
         layout.add_widget(lbl2)
-        layout.add_widget(lbl3)
+        layout.add_widget(self.lbl3)
         layout.add_widget(self.lbl4)
         layout.add_widget(self.lbl5)
         layout.add_widget(self.textinput)
@@ -97,21 +100,54 @@ class Screen2(Screen):
         layout.add_widget(layout2)
         self.add_widget(layout)
 
-    def inputtext(self, instance):
-        print("Переданный список из 1 класса",self.config)
-        t1 = self.textinput.text
-        if t1 == "0" or t1 == "1":
-            self.lbl5.text = f"введите, где стоит xxx: δ(q1, {t1}) = (xxx, *, *): "
-            t2 = self.textinput.text
-            if t2 == "0" or t2 == "1":
-                self.lbl5.text = "введите, где стоит xxx: δ(q1, {t1}) = ({t2}, xxx, *): "
-                t3 = self.textinput.text.lower()
-                if t3 == "r" or t3 == "l":
-                    self.lbl5.text = "введите, где стоит xxx: δ(q1, {t1}) = ({t2}, {t3}, xxx): "
-                    t4 = self.textinput.text
-                    if t4 == "q1" or t4 == "q0":
-                        print(f"δ(q¹, {t1}) = ({t2}, {t3}, {t4})")
-                        return t1, t2, t3, t4
+
+    def enter_rule(self, instance):
+        text = self.textinput.text.lower()
+        print('начали запись правила')
+
+        if len(self.rule) == 0 and text in ('0', '1'):
+            self.rule.append(text)
+            print('записали 1')
+            self.lbl5.text = f"введите, где стоит xxx: δ(q1, {self.rule[0]}) = (xxx, *, *): "
+        elif len(self.rule) == 1 and text in ('0', '1'):
+            self.rule.append(text)
+            print('записали 2')
+            self.lbl5.text = f"введите, где стоит xxx: δ(q1, {self.rule[0]}) = ({self.rule[1]}, xxx, *): "
+        elif len(self.rule) == 2 and text in ('r', 'l'):
+            self.rule.append(text)
+            print('записали 3')
+            self.lbl5.text = f"введите, где стоит xxx: δ(q1, {self.rule[0]}) = ({self.rule[1]}, {self.rule[2]}, xxx): "
+        elif len(self.rule) == 3 and text in ('q1', 'q0'):
+            self.rule.append(text)
+            print('записали 4')
+            self.lbl5.text = f"получено правило: δ(q1, {self.rule[0]}) = ({self.rule[1]}, {self.rule[2]}, {self.rule[3]}): "
+        print(self.rule)
+        self.textinput.text = ''
+
+    def apply_rule(self, instance):
+        pass
+
+
+
+
+
+
+    # def inputtext(self, instance):
+    #     print("Переданный список из 1 класса",self.config)
+    #     t1 = self.textinput.text
+    #     print(t1)
+    #     if t1 == "0" or t1 == "1":
+    #         self.lbl5.text = f"введите, где стоит xxx: δ(q1, {t1}) = (xxx, *, *): "
+    #         t2 = self.textinput.text
+    #     if t2 == "0" or t2 == "1":
+    #         self.lbl5.text = "введите, где стоит xxx: δ(q1, {t1}) = ({t2}, xxx, *): "
+    #         t3 = self.textinput.text.lower()
+    #     if t3 == "r" or t3 == "l":
+    #         self.lbl5.text = "введите, где стоит xxx: δ(q1, {t1}) = ({t2}, {t3}, xxx): "
+    #         t4 = self.textinput.text
+    #     if t4 == "q1" or t4 == "q0":
+    #         print(f"δ(q¹, {t1}) = ({t2}, {t3}, {t4})")
+    #     return t1, t2, t3, t4
 
     #def inputtext(self, instance):
     #    t1 = self.textinput.text
